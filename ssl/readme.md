55c1755933d542fbe8dc64af82b47ab65aee9c28
Stam He <stamhe@163.com>
2021-12-29 13:15:04 +0800

    新增ssl 使用说明

--- ssl/readme.md
+++ ssl/readme.md
@@ -2,6 +2,10 @@
 ```
 1. 安装
 apt install certbot
+
+新建目录，存放申请证书的域名认证字符串文件
+mkdir -p /data/www/common/.well-known/acme-challenge/
+
 2. 命令行执行
 申请单一子域名证书
 certbot certonly --manual -w /data/www/common -d test.stamhe.com
@@ -11,6 +15,15 @@ certbot certonly --manual  -d  *.stamhe.com --server https://acme-v02.api.letsen
 
 3. nginx 配置
 test_stamhe_com.conf
+
+4. 证书下来后，以后的正式使用，参考
+ip_stamhe_com.conf
+```
+
+# 自动更新
+```
+定时任务执行 ssl_renew.sh 搭配 ssl_authorize.sh 会自动更新证书
+注意：需要修改 ssl_authorize.sh 里面的信息为自己的
 ```
 
 
--- ssl/ssl_renew.sh
+++ ssl/ssl_renew.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # certbot certificates
 
-domain_list="www.google.com"
+domain_list="test.stamhe.com"
 
 for domain in $domain_list
 do
     #certbot certonly --manual --manual-public-ip-logging-ok --manual-auth-hook /data/ssl_authorize.sh -d "$domain" --dry-run 
-    certbot certonly --manual --keep-until-expiring --manual-public-ip-logging-ok --manual-auth-hook /data/ssl_authorize.sh -d "$domain"
+    certbot certonly --manual --keep-until-expiring --manual-auth-hook /data/ssl_authorize.sh -d "$domain"
 done
 
